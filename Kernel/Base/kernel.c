5099c6b2a3ded90c0c999ea598419433a4289332
omersaban169 <omersaban169@gmail.com>
2026-02-06 23:52:52 +0200

    hotfix: did so it wont switch to user mode just yet

--- Kernel/Base/kernel.c
+++ Kernel/Base/kernel.c
@@ -132,7 +132,7 @@ void obos_main()
     enablePagingNow();
     initKernelHeap();
     initUserHeap();
-    enterUserMode((void*)USER_SPACE_START);
+    // enterUserMode((void*)USER_SPACE_START);
     // // unsigned int* ptr = (unsigned int*)0xDEADBEEF;
     // unsigned int value = *ptr; //this will cause a page fault
     while (1);
