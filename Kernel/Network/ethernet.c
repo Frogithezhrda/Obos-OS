6c97a0b268348166bb6304aff2090be8decf80b4
omersaban169 <omersaban169@gmail.com>
2026-03-08 00:15:53 +0200

    feat: created just ethernet basic skeleton

---
+++ Kernel/Network/ethernet.c
@@ -0,0 +1,16 @@
+#include "ethernet.h"
+
+void ethernetInit()
+{
+    
+}
+
+void ethernetSend(void* data, unsigned int length)
+{
+
+}
+
+void ethernetReceive(void* buffer, unsigned int length)
+{
+
+}
