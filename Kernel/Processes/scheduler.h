ac2cc4860363e47f2fefbd61d1a3e2f286f9f9a3
Barakshhh <barak@idus.co.il>
2026-03-22 18:53:33 +0200

    fixed loadFirstProcess function signature and update related declarations

--- Kernel/Processes/scheduler.h
+++ Kernel/Processes/scheduler.h
@@ -5,11 +5,11 @@
 #include "switch.h"
 
 // The assembly function that performs the actual stack swap
-extern void switchTo(uint32_t* old_esp, uint32_t new_esp);
+extern void switch_to(uint32_t* old_esp, uint32_t new_esp);
 extern void start_first_process(uint32_t new_esp);
 
 void scheduler();
-void loadFirstProcess(PCB* pcb);
+void loadFirstProcess();
 
 
 //void tick(void);
