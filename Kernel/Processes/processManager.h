f6d9147a9d6daaffeaf2cb3409be6b112f2c610c
Barakshhh <barak@idus.co.il>
2026-03-22 11:52:53 +0200

    refactor process manager header for clarity and consistency

--- Kernel/Processes/processManager.h
+++ Kernel/Processes/processManager.h
@@ -2,67 +2,55 @@
 #define PROCESS_MANAGER_H
 
 #include "../Memory/heap.h"
+#include "../SystemLib/obosint.h"
 
 #define NULL 0
 #define TIME_SLICE 20
 
+#define EFLAGS_IF         0x00000200                    // Interrupt Enable
+#define EFLAGS_RESERVED   0x00000002                    // Mandatory bit (historically, bit 1 is always 1. If it's 0, some old processors get grumpy)
+#define DEFAULT_EFLAGS    (EFLAGS_IF | EFLAGS_RESERVED) // Default flags for a new process
+
 typedef enum States
 {
-    Ready = 0,
-    Running = 1,
-    Waiting = 2,
-    Terminated = 3,
-    Zombie = 4
+    Ready = 0,      // Sitting in the queue waiting for a turn.
+    Running = 1,    // Currently owning the CPU.
+    Waiting = 2     // Waiting for an input.
 } States;
 
-//currently only if flag interrupts and first bit enabled(intel says it must be)
+// Currently only if flag interrupts and first bit enabled(intel says it must be)
 typedef enum Flags
 {
     IF = 0x202
 } Flags;
 
-// CPU Context
-// edi
-// esi
-// ebx
-// ebp
-// eflags;
-    // This is pushed automatically by the C compiler's CALL instruction when switch_to() is called.
-// eip
 
+// Process Control Block
+// This structure holds all the information about a process.
 typedef struct PCB
 {
-    // Context Information
-    // This points to the CURRENT top of the stack for this process.
-    // When the process is running, this value is outdated.
-    // When the process is paused, this points to a CPU Context on the stack.
-    void* esp;
-
-    unsigned int propcessID;
-    unsigned int parentPID;
-    States state;
-    //all registers
-    CPUContext cpuContext;
-    //page directory
-
-    void* pageTableIndex;
-    unsigned int kernelStack;
-    unsigned int userStack;
-
-    //for scheduling
-    unsigned int timeSlice;
-
-    //like a linked list has prev node and next one
+    uint32_t esp;            // The current stack pointer (Saved during switch)
+    uint32_t pid;            // Process ID
+    States state;            // Process State
+    
+    void* pageTableIndex;    // CR3 register value for paging
+    
+    // Safety: Separate stacks
+    uint32_t kernelStack;    // Base of the kernel stack
+    uint32_t userStack;      // Base of the user stack (if you have one)
+    
+    uint32_t timeSlice;      // Ticks remaining for this process
+    
     struct PCB* prev;
     struct PCB* next;
 } PCB;
 
-//defining a queue
+// Queue structure for managing processes
 typedef struct ProcessQueue
 {
     PCB* head;
     PCB* tail;
-    unsigned int count;
+    uint32_t count;
 } ProcessQueue;
 
 extern PCB* currentProcess;
