bafe82873d96ab8a8bf9f070acd4c80f03a4ec65
Barakshhh <barak@idus.co.il>
2026-03-22 13:15:28 +0200

    implement process queue with init, push, pop, isQueueEmpty, and removeProcess functions

---
+++ Kernel/Processes/processQueue.c
@@ -0,0 +1,67 @@
+#include "processQueue.h"
+
+void init(ProcessQueue* q){
+    q->head = NULL;
+    q->tail = NULL;
+    q->size = 0;
+}
+
+void push(ProcessQueue* q, PCB* process){
+    if (!q || !process || isQueueEmpty(q)) {
+        process->prev = NULL;
+        process->next = NULL;
+        q->head = process;
+        q->tail = process;
+    } else {
+        q->tail->next = process;
+        process->prev = q->tail;
+        process->next = NULL;
+        q->tail = process;
+    }
+    q->size++;
+}
+
+PCB* pop(ProcessQueue* q){
+    if (isQueueEmpty(q)) {
+        return NULL;
+    }
+    PCB* headProcess = q->head;
+    if(!headProcess->next) {
+        q->head = NULL;
+        q->tail = NULL;
+    } else {
+        q->head = headProcess->next;
+        q->head->prev = NULL;
+    }
+    return headProcess;
+}
+
+int isQueueEmpty(ProcessQueue* q){
+    return q->size == 0;
+}
+
+void removeProcess(ProcessQueue* q, PCB* process){
+    if (!q || !process || isQueueEmpty(q)) {
+        return;
+    }
+
+    if (process->prev != NULL){
+        process->prev->next = process->next;
+    }
+    else {
+        q->head = process->next;
+    }
+
+    if (process->next != NULL){
+        process->nect->prev = process->prev;
+    }
+    else {
+        q->tail = proceess->prev;
+    }
+
+    process->next = NULL;
+    process->prev = NULL;
+
+    q->size--;
+}
+
---
+++ Kernel/Processes/processQueue.h
@@ -0,0 +1,21 @@
+#ifndef PROCESS_QUEUE_H
+#define PROCESS_QUEUE_H
+
+#include "../SystemLib/obosint.h"
+
+typedef struct PCB PCB;
+
+typedef struct ProcessQueue
+{
+    PCB* head;
+    PCB* tail;
+    uint32_t size;
+} ProcessQueue;
+
+void init(ProcessQueue* q);
+void push(ProcessQueue* q, PCB* process);
+PCB* pop(ProcessQueue* q);
+int isQueueEmpty(ProcessQueue* q);
+void removeProcess(ProcessQueue* q, PCB* process);
+
+#endif
